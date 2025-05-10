62449e706460bab77ad66adbe1317c889b247594
Ayyan <naveedayyan8@gmail.com>
2025-05-10 18:05:02 +0500

    sidebar updated

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');    
 const video3 = document.getElementById('projectVideo3');
 const hoverSign = document.querySelector('.hover-sign');
+// sidebar element
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const close = document.querySelector('.close-icon');
 const videoList = [video1, video2, video3];
 
 
@@ -14,4 +18,18 @@ videoList.forEach((video) => {
         video.pause();
         hoverSign.classList.remove('active');
     })
+})
+
+// sidebar element
+
+menu.addEventListener('click', function() { 
+    sideBar.classList.add("open-sidebar");
+    sideBar.classList.remove("close-sidebar");
+    console.log('sidebar opened');
+})
+
+close.addEventListener('click', function() {
+    sideBar.classList.add("close-sidebar");
+    sideBar.classList.remove("open-sidebar");
+    console.log('sidebar closed');
 })
